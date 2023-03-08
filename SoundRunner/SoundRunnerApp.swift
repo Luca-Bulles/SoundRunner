2b5cb3c07be7f97e65bf5c8356cae12200fbd3b0
Luca Bulles <luca@lucas-mbp.home>
2023-03-08 15:22:32 +0100

    added project

---
+++ SoundRunner/SoundRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundRunnerApp.swift
+//  SoundRunner
+//
+//  Created by Luca Bulles on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SoundRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
